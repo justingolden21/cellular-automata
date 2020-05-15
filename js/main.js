cb5be44e80f398aede1f9c9f5de4d8a393ea5c30
Justin Golden <justingolden21@gmail.com>
2020-05-14 22:40:51 -0700

    added input verification, fixed typo

--- js/main.js
+++ js/main.js
@@ -2,11 +2,17 @@ $( ()=> {
 	$('#rule-num-input').select();
 	$('#rule-num-input').change( ()=> {
 		let num = parseInt($('#rule-num-input').val() );
+		num = verify(num, 0, 255, 0);
+		$('#rule-num-input').val(num);
 
 		$('#display').html('');
 		drawRuleDisplay(num);
 		drawCellularDisplay(num);
-
 		document.title = `Cellular Automata - Rule ${num}`;
 	}).change();
 });
+
+function verify(num, min, max, defaultVal) {
+	num = Math.max(Math.min(parseInt(num),max),min);
+	return isNaN(num) ? defaultVal : num;
+}
