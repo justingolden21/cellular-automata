746db4b36aff62c4070716d053868872558e9f9f
Justin Golden <justingolden21@gmail.com>
2021-01-10 00:50:43 -0800

    moved some settings to new modal
    updated modal code for multiple modals as well

--- js/modal.js
+++ js/modal.js
@@ -1,26 +1,36 @@
 $( ()=> {
 
-	let modal = document.getElementById('about-modal');
-	let btn = document.getElementById('about-modal-btn');
-	let close = document.getElementsByClassName('close')[0];
+	let modals = document.getElementsByClassName('modal');
+	let modalBtns = document.getElementsByClassName('modal-btn');
+	let closeBtns = document.getElementsByClassName('close');
 
-	btn.onclick = function() {
-		modal.style.display = 'block';
+	for(let modalBtn of modalBtns) {
+		modalBtn.onclick = function(event) {
+			document.querySelector(event.target.getAttribute('href') ).style.display = 'block';
+		}
 	}
 
-	close.onclick = function() {
-		modal.style.display = 'none';
+	for(let closeBtn of closeBtns) {
+		closeBtn.onclick = function(event) {
+			event.target.parentNode.parentNode.style.display = 'none';
+		}
 	}
 
 	window.onclick = function(event) {
-		if (event.target == modal) {
-			modal.style.display = 'none';
+		if(event.target.classList.contains('modal') ) {
+			for(let modal of modals) {
+				if(typeof modal.style !== 'undefined') {
+					modal.style.display = 'none';    
+				}
+			}
 		}
 	}
 
 	window.onkeydown = function(event) {
 		if (event.key == 'Escape') {
-			modal.style.display = 'none';
+			for(let modal of modals) {
+				modal.style.display = 'none';
+			}
 		}
 	}
 
